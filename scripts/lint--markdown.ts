a4eeb9c8e56a0e3d12ecde4a7f3e8bbefdb378d6
John Lianoglou <john@prometheas.com>
2026-02-20 22:11:13 -0500

    feat: add markdown linting script

---
+++ scripts/lint--markdown.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S deno run --allow-run --allow-read
+
+const command = new Deno.Command("deno", {
+  args: [
+    "run",
+    "--allow-read",
+    "--allow-sys",
+    "npm:markdownlint-cli2",
+    "--config",
+    ".config/markdownlint.json",
+    "**/*.md",
+  ],
+  stdout: "inherit",
+  stderr: "inherit",
+});
+
+const { code } = await command.output();
+Deno.exit(code);
